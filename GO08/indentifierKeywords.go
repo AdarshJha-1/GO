a0a0f09469b57c0736c8ec80ce00fd870b5e7279
Adarsh Jha <adarsh2005j@gmail.com>
2024-10-30 22:45:00 +0530

    keywords go

---
+++ GO08/indentifierKeywords.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myInt int = 10 // snake case
+	fmt.Println(myInt)
+
+	var _myInt int = 110
+	fmt.Println(_myInt)
+
+	var MyInt int = 1110
+	fmt.Println(MyInt)
+}
