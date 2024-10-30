145b19732e29652547aa3932a446c4aaac4d5107
Adarsh Jha <adarsh2005j@gmail.com>
2024-10-30 23:05:29 +0530

    go going

---
+++ GO09/operators.go
@@ -0,0 +1,41 @@
+package main
+
+func main() {
+	/*
+		10 + 5
+		10, 5 are operands
+		+ is operator
+	*/
+
+	// Unary Operators
+	// +, - , !, ^, ++, --, *, &
+
+	// Binary Operators
+
+	// Arithmetic Operators
+	// +, -, *, /, % use with two operands
+
+	// Bitwise Logical & Shift Operators
+	// &, |, ^, &^, <<, >>
+
+	// Comparison Operators
+	// ==, !=, <, >, <=, >=
+
+	// Logical Operators
+	// &&, ||, !
+
+	/*
+		Other Operators
+		& : address operator
+		* : de-referencing operator
+		<- : receive value from channel
+
+	*/
+
+	/*
+		Operator Precedence
+		P E M D A S
+		Parentheses, Exponents, Multiplication, Addition, Subtraction
+	*/
+
+}
