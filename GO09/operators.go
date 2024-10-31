229ace463c749be276d46c1c2f56facec378b81d
Adarsh Jha <adarsh2005j@gmail.com>
2024-10-31 15:01:04 +0530

    operators go

--- GO09/operators.go
+++ GO09/operators.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	/*
 		10 + 5
@@ -7,13 +9,62 @@ func main() {
 		+ is operator
 	*/
 
+	// ----------------------------------------------------------------//
 	// Unary Operators
 	// +, - , !, ^, ++, --, *, &
+	num1 := 19
+	fmt.Println(num1)
+	num1++
+	fmt.Println(num1)
+	num1--
+	fmt.Println(num1)
 
-	// Binary Operators
+	/*
+		// Binary Operators
 
-	// Arithmetic Operators
-	// +, -, *, /, % use with two operands
+		// Arithmetic Operators
+		// +, -, *, /, % use with two operands
+		var a, b int = 10, 4
+		var c float64 = 69.69
+
+		sum := a + b
+		fmt.Println(sum)
+
+		firstName := "Adarsh"
+		lastName := "Jha"
+		// fullName := firstName + " " + lastName
+		fullName := fmt.Sprintf("%s %s", firstName, lastName)
+		fmt.Println(fullName)
+
+		sub := a - b
+		fmt.Println(sub)
+
+		mul := a * b
+		fmt.Println(mul)
+
+		div := a / b
+		fmt.Println(div)
+
+		remainder := a % b // remainder operator can only be used with int or uint
+		fmt.Println(remainder)
+
+		newSum := a + int(c) // type conversion
+		fmt.Println(newSum)
+
+		z := 10
+		// z = z + 5
+		z += 5 // shorthand ope. +=, -= , *=, /=
+		fmt.Println(z)
+
+		z -= 4
+		fmt.Println(z)
+
+
+			// zeroDiv := a / 0
+			// fmt.Println(zeroDiv)
+			// # command-line-arguments
+			// GO09/operators.go:57:17: invalid operation: division by zero
+	*/
 
 	// Bitwise Logical & Shift Operators
 	// &, |, ^, &^, <<, >>
